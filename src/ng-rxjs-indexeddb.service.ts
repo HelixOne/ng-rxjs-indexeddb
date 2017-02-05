7f25e16f6bbe26ad5c7340da148129aa60c532f5
Zubair Lawrence <zubair@affectlayer.com>
2017-02-04 23:44:38 -0800

    init

---
+++ src/ng-rxjs-indexeddb.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, EventEmitter, Output } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import * as idbKeyVal from 'idb-keyval';
+
+@Injectable()
+export class NgRxjsIndexeddbService {
+
+    private _data = {}
+   
+    constructor() { }
+
+
+    init(key) {
+        if (this._data[key]) {
+            return
+        }
+        this._data[key] = new BehaviorSubject(null);
+        addEventListener('storage', (e: StorageEvent) => {
+            let key = e.key.slice(18)
+            if (key && this._data[key]) {
+                this.get(key)
+            }
+        }, false);
+
+    }
+    set(key, value) {
+        this.init(key)
+        idbKeyVal.set(key, value)
+            .then(() => {
+                this._data[key].next(value)
+            })
+        localStorage.setItem('ng-rxjs-indexeddb-' + key, Math.random().toString())
+    }
+    get(key) {
+        this.init(key)
+        idbKeyVal.get(key)
+            .then((value) => {
+                this._data[key].next(value)
+            })
+        return this.watch(key)
+    }
+    del(key) {
+        this.init(key)
+        idbKeyVal.get(key)
+            .then(() => {
+                this._data[key].next(null)
+            })
+    }
+    private watch(key) {
+        this.init(key)
+        return new Observable(fn => this._data[key].subscribe(fn));
+    }
+
+}
